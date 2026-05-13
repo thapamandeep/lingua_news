9c8fd2df85ceb3051ff90579d30afb50480162dd
thapamandeep <thapamandeep069@gmail.com>
2026-05-13 14:39:48 +0545

    header complete

---
+++ resources/views/fronted/partials/header.blade.php
@@ -0,0 +1,33 @@
+<header class="site-header">
+    <div class="background">
+
+    <div class="media-icon">
+        <img  class="img-icon"src="{{asset('img/twitter-1.png')}}" alt="icon-image">
+        <img  class="img-icon"src="{{asset('img/insta-1.png')}}" alt="icon-image">
+        <img  class="img-icon"src="{{asset('img/facebook-1.png')}}" alt="icon-image">
+        <img  class="img-icon"src="{{asset('img/youtube-1.png')}}" alt="icon-image">
+
+        <div class="news-head">
+            <h2 class="head">Lingua News</h2>
+        </div>
+
+        <div class="date">
+            <input class="select-date" type="date">
+        </div>
+
+        <div class="search-bar">
+    <input type="text" placeholder="Search news...">
+    <i class="fa fa-search search-icon"></i>
+     </div>
+
+     <div class="signin">
+        <input type="text" placeholder="sign in">
+           <i class="fa fa-user user-icon"></i>
+     </div>
+
+    </div>
+
+    </div>
+    <hr>
+
+</header>
