11bf12b3b574f541525c2e804deefd47f0008a93
thapamandeep <thapamandeep069@gmail.com>
2026-05-15 17:39:58 +0545

    admin start

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,13 @@ use App\Http\Controllers\SiteController;
 use App\Http\Controllers\SportsController;
 use App\Http\Controllers\PoliticsController;
 use App\Http\Controllers\EntertainmentsController;
+use App\Http\Controllers\AdminController;
 
+// ---------------------------site----------------------------------//
 Route::get('/',[SiteController::class,'home'])->name('home.index');
 Route::get('/sport',[SportsController::class,'index'])->name('sports.index');
 Route::get('/politics',[PoliticsController::class,'index'])->name('politics.index');
 Route::get('/entertainments',[EntertainmentsController::class,'index'])->name('entertainments.index');
+
+// ----------------------------Admin----------------------------------//
+Route::get('/admin-dashboard',[AdminController::class,'index'])->name('admin.dashboard');
